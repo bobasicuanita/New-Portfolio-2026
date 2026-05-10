3160314f45fce33cc6160b4ca4718479870184f5
unknown <konstantinoskarasavvas@hotmail.com>
2026-05-10 20:18:07 +0300

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { Outfit } from "next/font/google";
 import { ThemeProvider } from "next-themes";
@@ -50,6 +51,7 @@ export default function RootLayout({
           </div>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
